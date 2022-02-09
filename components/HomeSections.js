8ddd62b5111349b5ab3143413fea2b0a10c80faa
devSmartAgence <dev@smartagence.com>
2022-02-09 16:22:35 +0100

    Deploy netlify

--- components/HomeSections.js
+++ components/HomeSections.js
@@ -1,5 +1,5 @@
-import ContentPanel from "./ContentPanel";
-import CoverPanel from "./CoverPanel";
+import ContentPanel from "ContentPanel";
+import CoverPanel from "CoverPanel";
 import HomeSplitPanelControl from "./ui/HomeSplitPanelControl";
 import GridPattern from "./ui/GridPattern";
 
